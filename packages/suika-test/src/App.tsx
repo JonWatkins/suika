df31052c0f14a4836d53bd6962182a32f376d0da
Jon <jonwatkins86@gmail.com>
2023-06-08 21:58:07 +0100

    Fix router URL parsing

--- packages/suika-test/src/App.tsx
+++ packages/suika-test/src/App.tsx
@@ -6,7 +6,8 @@ export class App extends Component {
   render(): vNode {
     return (
       <div id="container">
-        <RouterLink to="/">Counter</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/counter">Counter</RouterLink>
         <RouterLink to="/404">404 Error</RouterLink>
         <RouterView router={router} />
       </div>
---
+++ packages/suika-test/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import { Component, vNode, h, Fragment } from "suika";
+
+export class Home extends Component {
+  render(): vNode {
+    return (
+      <>
+        <h1>Hello world</h1>
+      </>
+    );
+  }
+}
