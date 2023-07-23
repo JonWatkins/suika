a4b069e4bb046378a10e17e28db1ad48ee8dde39
Jon Watkins <jonwatkins86@gmail.com>
2023-07-23 23:44:34 +0100

    Added more styling

--- packages/suika-test/src/routes/Home.tsx
+++ packages/suika-test/src/routes/Home.tsx
@@ -4,28 +4,45 @@ export class Home extends Component {
   render(): vNode {
     return (
       <div className="container-xl mx-auto">
-        <section className="dark:bg-light-600">
+        <section className="dark:bg-light-600 mb-1.5">
           <div className="sm:container-xs md:container-md xl:container-lg mx-auto">
             <div className="py-4 px-2 lg:py-8 lg:px-4 mx-auto flex-col justify-center items-center">
               <h1 className="text-5xl">Lorem ipsum dolor</h1>
-              <p className="text-xl mb-1.5 text-justify">
+              <p className="text-xl mb-1.5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
               <div className="flex-row justify-center items-center">
-                <a
-                  className="bg-primary-500 py-0.5 px-1 rounded-md text-2xl mr-1.5 dark:text-light-500"
-                  href="#"
-                >
+                <a className="btn btn-primary mr-1.5" href="#">
                   Learn More
                 </a>
-                <a
-                  className="bg-secondary-500 py-0.5 px-1 rounded-md text-2xl mr-1.5 dark:text-light-500 py-0.5 px-1 rounded-md sm:none md:block"
-                  href="#"
-                >
+                <a className="btn btn-secondary" href="#">
                   Sign Up
                 </a>
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex-row">
+          <div className="card mr-1.5">
+            <div className="card-header">Lorem ipsum</div>
+            <div className="card-body">
+              <h2 className="card-title">Lorem ipsum</h2>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">Lorem ipsum</div>
+            <div className="card-body">
+              <h2 className="card-title">Lorem ipsum</h2>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
             </div>
           </div>
         </section>
