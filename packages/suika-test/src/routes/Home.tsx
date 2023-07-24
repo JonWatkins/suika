8cddc4cd696844773320041959c3e41a83018728
Jon Watkins <jonwatkins86@gmail.com>
2023-07-24 02:43:51 +0100

    updating scss and test app

--- packages/suika-test/src/routes/Home.tsx
+++ packages/suika-test/src/routes/Home.tsx
@@ -6,7 +6,7 @@ export class Home extends Component {
       <div className="container-xl mx-auto">
         <section className="dark:bg-light-600 mb-1.5">
           <div className="sm:container-xs md:container-md xl:container-lg mx-auto">
-            <div className="py-4 px-2 lg:py-8 lg:px-4 mx-auto flex-col justify-center items-center">
+            <div className="py-4 px-2 lg:py-8 lg:px-4 mx-auto flex-col text-center">
               <h1 className="text-5xl">Lorem ipsum dolor</h1>
               <p className="text-xl mb-1.5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -24,8 +24,8 @@ export class Home extends Component {
           </div>
         </section>
 
-        <section className="flex-row">
-          <div className="card mr-1.5">
+        <section className="flex-col md:flex-row m-1.5 xl:m-0">
+          <div className="card mb-1.5 md:mb-0 md:mr-1.5">
             <div className="card-header">Lorem ipsum</div>
             <div className="card-body">
               <h2 className="card-title">Lorem ipsum</h2>
